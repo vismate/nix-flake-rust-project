36bf7e5eea00c3b21089f4f72904d71ba8c4351c
vismate <vismate@vismate.xyz>
2024-12-20 20:59:19 +0100

    chore: set up repo

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Rust Project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+
+      rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      pname = manifest.name;
+      version = manifest.version;
+
+      nativeBuildInputs = [ rustToolchain ];
+      buildInputs = [ ];
+    in
+    {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        inherit pname version nativeBuildInputs buildInputs;
+
+        meta = {
+          name = pname;
+          version = version;
+          description = manifest.description;
+          licence = manifest.license;
+          authors = manifest.authors;
+        };
+
+        src = pkgs.nix-gitignore.gitignoreSource [] ( pkgs.lib.cleanSource ./. );
+        cargoLock.lockFile = ./Cargo.lock;
+
+        cargoSha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+        doCheck = true;
+      };
+
+      devShell = pkgs.mkShell {
+        inherit buildInputs nativeBuildInputs;
+        RUST_BACKTRACE=1;
+        LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
+      };
+    });
+}
